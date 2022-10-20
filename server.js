3cf13be80adb5d526652937faa29cd60b7050803
Clay Loftus <clay.loftus@flatsorspikes.com>
2022-10-20 17:35:31 -0600

    Set up MongoDB connection

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 'use strict';
 
+// Import modules
 const express = require('express');
+const mongoose = require('mongoose');
 const { createUser } = require('./user.js')
 
+// Launch app
 const app = express();
 const PORT = 3000;
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
-// Request Handlers
+// Connect to database and test connection
+require('dotenv').config();
+const mongoString = process.env.DATABASE_URL
 
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// Request Handlers
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`)
 });
